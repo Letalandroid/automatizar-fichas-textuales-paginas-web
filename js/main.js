b10c79240dc5e9dd6d00d1302be58e77cb2b8579
Letalandroid <carlossoncra@protonmail.com>
2021-11-13 12:59:41 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+let send = document.getElementById("send").addEventListener("click", () => {
+
+    let lastname = document.getElementById("lastname").value;
+    let name = document.getElementById("name").value;
+    let fecha = document.getElementById("fecha").value;
+    let title = document.getElementById("title").value;
+    let namePage = document.getElementById("namePage").value;
+    let link = document.getElementById("linkPage").value;
+
+    let resultado = '<p>' + lastname + ', ' + name.charAt(0) + '.' + '(' + fecha + ').' +
+        '<i>' + title + '.</i>' + namePage + '. ' + link;
+
+
+    document.getElementById("resultado").innerHTML = resultado;
+
+});
